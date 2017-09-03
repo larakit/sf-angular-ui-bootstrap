adf193a45c9bb6998862d25fab170d06bf9e7d39
Alexey Berdnikov <aberdnikov@gmail.com>
2017-09-03 16:13:29 +0500

    fixed

--- public/template/carousel/carousel.html.js
+++ public/template/carousel/carousel.html.js
@@ -2,11 +2,11 @@ angular.module("uib/template/carousel/carousel.html", []).run(["$templateCache",
   $templateCache.put("uib/template/carousel/carousel.html",
     "<div class=\"carousel-inner\" ng-transclude></div>\n" +
     "<a role=\"button\" href class=\"left carousel-control\" ng-click=\"prev()\" ng-class=\"{ disabled: isPrevDisabled() }\" ng-show=\"slides.length > 1\">\n" +
-    "  <span aria-hidden=\"true\" class=\"glyphicon glyphicon-chevron-left\"></span>\n" +
+    "  <span aria-hidden=\"true\" class=\"fa fa-chevron-left\"></span>\n" +
     "  <span class=\"sr-only\">previous</span>\n" +
     "</a>\n" +
     "<a role=\"button\" href class=\"right carousel-control\" ng-click=\"next()\" ng-class=\"{ disabled: isNextDisabled() }\" ng-show=\"slides.length > 1\">\n" +
-    "  <span aria-hidden=\"true\" class=\"glyphicon glyphicon-chevron-right\"></span>\n" +
+    "  <span aria-hidden=\"true\" class=\"fa fa-chevron-right\"></span>\n" +
     "  <span class=\"sr-only\">next</span>\n" +
     "</a>\n" +
     "<ol class=\"carousel-indicators\" ng-show=\"slides.length > 1\">\n" +
